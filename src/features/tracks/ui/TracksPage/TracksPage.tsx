b1826f98efeacacb6c42c8eefef9d3183ceef1a4
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-09-23 10:55:32 +0300

    fix: decomposition to TrackPAge

--- src/features/tracks/ui/TracksPage/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage/TracksPage.tsx
@@ -1,84 +1,23 @@
 import {useFetchTracksInfiniteQuery} from "@/features/tracks/api/trackApi.ts";
-import s from"./TracksPage.module.css"
-import {useCallback, useEffect, useRef} from "react";
+import {useInfiniteScroll} from "@/common/hooks/useInfiniteScroll.ts";
+import {TracksList} from "@/features/tracks/ui/TracksList/TracksList.tsx";
+import {LoadingTrigger} from "@/features/tracks/ui/LoadingTrigger/LoadingTrigger.tsx";
 
 export const TracksPage = () => {
+    const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } =
+        useFetchTracksInfiniteQuery()
 
-    const { data, isLoading, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } = useFetchTracksInfiniteQuery()
+    const { observerRef } = useInfiniteScroll({ hasNextPage, isFetching, fetchNextPage })
 
-    const observerRef = useRef<HTMLDivElement>(null)
-
-    // const pages = data?.pages.map(page => page.data).flat() || []
-    const pages = data?.pages.flatMap((page) => page.data) || []
-
-    const loadMoreHandler = useCallback (() => {
-        if (hasNextPage && !isFetching) {
-        fetchNextPage()
-        }
-    }, [hasNextPage, isFetching, fetchNextPage])
-
-    useEffect(() => {
-        // IntersectionObserver отслеживает элементы и сообщает, насколько они видны во viewport
-        const observer = new IntersectionObserver(
-            entries => {
-                // entries - наблюдаемый элемент
-                if (entries.length > 0 && entries[0].isIntersecting) {
-                    loadMoreHandler()
-                }
-            },
-            {
-                root: null, // Отслеживание относительно окна браузера (viewport). null = весь экран
-                rootMargin: '100px', // Начинать загрузку за 100px до появления элемента
-                threshold: 0.1, // Срабатывать когда 10% элемента становится видимым
-            }
-        )
-
-        const currentObserverRef = observerRef.current
-        if (currentObserverRef) {
-            // начинает наблюдение за элементом
-            observer.observe(currentObserverRef)
-        }
-
-        // Функция очистки - прекращает наблюдение при размонтировании компонента
-        return () => {
-            if (currentObserverRef) {
-                observer.unobserve(currentObserverRef)
-            }
-        }
-    }, [loadMoreHandler])
+    const pages = data?.pages.flatMap(page => page.data) || []
 
     return (
         <div>
             <h1>Tracks page</h1>
-            <div className={s.list}>
-                {pages.map(track => {
-                    const { title, user, attachments } = track.attributes
-
-                    return (
-                        <div key={track.id} className={s.item}>
-                            <div>
-                                <p>Title: {title}</p>
-                                <p>Name: {user.name}</p>
-                            </div>
-                            {attachments.length ? <audio controls src={attachments[0].url} /> : 'no file'}
-                        </div>
-                    )
-                })}
-            </div>
+            <TracksList tracks={pages} />
             {hasNextPage && (
-                // Этот элемент отслеживается IntersectionObserver
-                <div ref={observerRef}>
-                    {/*`<div style={{ height: '20px' }} />` создает "невидимую зону" в 20px в конце списка,*/}
-                    {/*при достижении которой автоматически загружаются новые треки. Без размеров*/}
-                    {/*IntersectionObserver не будет работать корректно.*/}
-                    {isFetchingNextPage ? (
-                        <div>Loading more tracks...</div>
-                    ) : (
-                        <div style={{ height: '20px' }} />
-                    )}
-                </div>
+                <LoadingTrigger isFetchingNextPage={isFetchingNextPage} observerRef={observerRef} />
             )}
-
             {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
         </div>
     )
