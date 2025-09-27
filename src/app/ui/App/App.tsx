e5c7cc414b97bfceabadeff3250e629fdc38f770
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-09-27 11:33:59 +0300

    fix: under LinearProgress the Headwr

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -2,12 +2,16 @@ import {Routing} from "@/common/routing/Routing.tsx";
 import {Header} from "@/common/components/components/Header";
 import s from './App.module.css'
 import {ToastContainer} from "react-toastify";
+import {useGlobalLoading} from "@/common/hooks/useGlobalLoading.ts";
+import {LinearProgress} from "@/common/components/components/LinearProgress/LinearProgress.tsx";
 
 
 export const App = () => {
+    const isGlobalLoading = useGlobalLoading()
     return (
         <>
             <Header/>
+            {isGlobalLoading && <LinearProgress />}
             <div className={s.layout}>
                 <Routing/>
             </div>
