e9bbd9d257b4bde714b3507370408b5508587099
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-10-03 17:03:18 +0300

    feat: added endpoint login

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,10 +1,17 @@
 import {baseApi} from "@/app/api/baseApi.ts";
-import type {MeResponse} from "@/features/auth/api/authApi.types.ts";
+import type {LoginArgs, LoginResponse, MeResponse} from "@/features/auth/api/authApi.types.ts";
 
 export const authApi = baseApi.injectEndpoints({
     endpoints: build => ({
         getMe: build.query<MeResponse, void>({
             query: () => `auth/me`,
+        }),
+        login: build.mutation<LoginArgs, LoginResponse>({
+            query: payload => ({
+                url: 'auth/login',
+                method: 'post',
+                body: {...payload, accessToken: '3m'}
+            }),
         }),
     }),
 })
