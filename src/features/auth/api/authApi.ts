d09b9a21fe52c7ce3cc4826fd2f176bd7cf19d20
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-10-03 17:20:55 +0300

    feat: added oauth2

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,5 +1,6 @@
 import {baseApi} from "@/app/api/baseApi.ts";
 import type {LoginArgs, LoginResponse, MeResponse} from "@/features/auth/api/authApi.types.ts";
+import {AUTH_KEYS} from "@/common/constants/constants.ts";
 
 export const authApi = baseApi.injectEndpoints({
     endpoints: build => ({
@@ -8,10 +9,17 @@ export const authApi = baseApi.injectEndpoints({
         }),
         login: build.mutation<LoginResponse, LoginArgs>({
             query: payload => ({
-                url: 'auth/login',
+                url: `auth/login`,
                 method: 'post',
-                body: {...payload, accessToken: '3m'}
+                body: { ...payload, accessTokenTTL: '3m' },
             }),
+            async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
+                const { data } = await queryFulfilled
+                localStorage.setItem(AUTH_KEYS.accessToken, data.accessToken)
+                localStorage.setItem(AUTH_KEYS.refreshToken, data.refreshToken)
+                // Invalidate after saving tokens
+                dispatch(authApi.util.invalidateTags(['Auth']))
+            },
         }),
     }),
 })
